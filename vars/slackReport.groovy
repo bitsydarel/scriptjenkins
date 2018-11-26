51f89b8ff05ce88f68ac34674c42cc6ff3f9a3e6
Darel Bitsy <bitsydarel@gmail.com>
2018-11-26 03:59:02 +0200

    changed json generator library to the default javax other then net.sf.json. because of the strange formatting.

--- vars/slackReport.groovy
+++ vars/slackReport.groovy
@@ -1,14 +1,14 @@
 import hudson.tasks.junit.CaseResult
 import hudson.tasks.test.AbstractTestResultAction
-import net.sf.json.JSONArray
-import net.sf.json.JSONObject
+
+import javax.json.*
 
 final def notifySlackWithPlugin(final text, final channel, final attachments) {
     echo 'sending report to slack'
     slackSend(message: text, channel: channel, attachments: attachments)
 }
 
-final def getGitAuthor() {
+final String getGitAuthor() {
     echo 'getting git author'
     final def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
     return sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${commit}").trim()
@@ -42,7 +42,7 @@ final String getTestSummary() {
 @NonCPS
 final String getFailedTests() {
     final def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-    def failedTestsString = "```"
+    String failedTestsString = "```"
 
     if (testResultAction != null) {
         def failedTests = testResultAction.getFailedTests()
@@ -68,22 +68,22 @@ final int getFailedTestCount() {
 final boolean isPublishingBranch() { return env.GIT_BRANCH == 'origin/master' }
 
 final def call(final String slackChannel) {
-    final def message = getLastCommitMessage()
-    final def author = getGitAuthor()
-    final def testSummary = getTestSummary()
+    final String message = getLastCommitMessage()
+    final String author = getGitAuthor()
+    final String testSummary = getTestSummary()
 
-    def buildColor = currentBuild.result == null ? "good" : "warning"
-    def buildStatus = currentBuild.result == null ? "Success" : currentBuild.result
+    String buildColor = currentBuild.result == null ? "good" : "warning"
+    String buildStatus = currentBuild.result == null ? "Success" : currentBuild.result
     String jobName = "${env.JOB_NAME}"
 
     // Strip the branch name out of the job name (ex: "Job Name/branch1" -> "Job Name")
     jobName = jobName[0..(jobName.indexOf('/') - 1)]
 
-    final JSONArray attachments = new JSONArray()
+    final JsonArrayBuilder attachments = Json.createArrayBuilder()
 
-    final JSONObject defaultMessage = new JSONObject()
-    defaultMessage.put("title", "${jobName}, build #${env.BUILD_NUMBER}")
-    defaultMessage.put("title_link", "${env.BUILD_URL}")
+    final JsonObjectBuilder defaultMessage = Json.createObjectBuilder()
+    defaultMessage.add("title", "${jobName}, build #${env.BUILD_NUMBER}")
+    defaultMessage.add("title_link", "${env.BUILD_URL}")
 
     if (getFailedTestCount() > 0) {
         buildStatus = "Failed"
@@ -94,64 +94,65 @@ final def call(final String slackChannel) {
 
         buildColor = "danger"
 
-        defaultMessage.put("color", "${buildColor}")
-        defaultMessage.put("text", "${buildStatus}\n${author}")
+        defaultMessage.add("color", buildColor)
+        defaultMessage.add("text", "${buildStatus}\n${author}")
         //markdown location
-        final JSONArray fieldsMarkdowns = new JSONArray()
-        fieldsMarkdowns.add("fields")
-        defaultMessage.put("mrkdwn_in", fieldsMarkdowns)
+        defaultMessage.add("mrkdwn_in", Json.createArrayBuilder().add("fields"))
         //populating all fields
-        defaultMessage.put("fields", getDefaultFields(testSummary, message))
+        defaultMessage.add("fields", getDefaultFields(testSummary, message))
 
         attachments.add(defaultMessage)
 
-        final JSONArray textMarkdowns = new JSONArray()
-        fieldsMarkdowns.add("text")
-
-        final JSONObject failedTests = new JSONObject()
-        failedTests.put("title", "Failed Tests")
-        failedTests.put("color", "${buildColor}")
-        failedTests.put("text", "${getFailedTests()}")
-        failedTests.put("mrkdwn_in", textMarkdowns)
-
-        attachments.add(failedTests)
+        attachments.add(
+                Json.createObjectBuilder()
+                        .add("title", "Failed Tests")
+                        .add("color", buildColor)
+                        .add("text", getFailedTests())
+                        .add("mrkdwn_in", Json.createArrayBuilder().add("text"))
+        )
     } else {
-        defaultMessage.put("color", "${buildColor}")
-        defaultMessage.put("author_name", "${author}")
-        defaultMessage.put("text", "${buildStatus}\n${author}")
-        defaultMessage.put("fields", getDefaultFields(testSummary, message))
+        defaultMessage.add("color", buildColor)
+        defaultMessage.add("author_name", "${author}")
+        defaultMessage.add("text", "${buildStatus}\n${author}")
+        defaultMessage.add("fields", getDefaultFields(testSummary, message))
 
         attachments.add(defaultMessage)
     }
 
-    echo attachments.toString()
+    final StringWriter writer = new StringWriter()
+    final JsonArray result = attachments.build()
+    Json.createWriter(writer)..withCloseable { jsonWriter ->
+        jsonWriter.writeArray(result)
+    }
 
-    notifySlackWithPlugin("", slackChannel, attachments.toString())
+    echo writer.toString()
+
+    notifySlackWithPlugin("", slackChannel, writer.toString())
 }
 
-final JSONArray getDefaultFields(final String testSummary, final String lastCommitMessage) {
-    final JSONArray defaultFields = new JSONArray()
+final JsonArray getDefaultFields(final String testSummary, final String lastCommitMessage) {
+    final JsonArrayBuilder defaultFields = Json.createArrayBuilder()
 
-    final JSONObject branchField = new JSONObject()
-    branchField.put("title", "Branch")
-    branchField.put("value", "${env.GIT_BRANCH}")
-    branchField.put("short", true)
+    defaultFields.add(
+            Json.createObjectBuilder()
+                    .add("title", "Branch")
+                    .add("value", "${env.GIT_BRANCH}")
+                    .add("short", true)
+    )
 
-    defaultFields.add(branchField)
+    defaultFields.add(
+            Json.createObjectBuilder()
+                    .add("title", "Test Results")
+                    .add("value", testSummary)
+                    .add("short", true)
+    )
 
-    final JSONObject testResults = new JSONObject()
-    testResults.put("title", "Test Results")
-    testResults.put("value", "${testSummary}")
-    testResults.put("short", true)
+    defaultFields.add(
+            Json.createObjectBuilder()
+                    .add("title", "Last Commit")
+                    .add("value", lastCommitMessage)
+                    .add("short", false)
+    )
 
-    defaultFields.add(testResults)
-
-    final JSONObject lastCommit = new JSONObject()
-    lastCommit.put("title", "Last Commit")
-    lastCommit.put("value", "${lastCommitMessage}")
-    lastCommit.put("short", false)
-
-    defaultFields.add(lastCommit)
-
-    return defaultFields
+    return defaultFields.build()
 }
